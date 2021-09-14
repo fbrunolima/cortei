e480186fbe9be82e676f650f3213b8b5837d2e2d
Felipe Bruno <ti.fbrunolima@gmail.com>
2021-09-13 21:36:13 -0300

    Inicio Projeto Cortei

---
+++ src/main/java/com/example/demo/CorteiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorteiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorteiApplication.class, args);
+	}
+
+}
